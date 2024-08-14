f88022faf0b5b1d30b007b9197bdbcab20a2eb0c
Bruno Apolinário <bruno.apolinario010@gmail.com>
2024-08-14 10:04:26 -0300

    fix: corrigido typo aws_parameters | feat: componente google drive

--- components/aws_parameters.py
+++ components/aws_parameters.py
@@ -1,6 +1,6 @@
 import boto3
 
-def get_regin_name():
+def get_region_name():
     ssm = boto3.client('ssm', region_name='sa-east-1') #definindo região padrão
     try:
         parameter = ssm.get_parameter(Name='/human/REGION_NAME', WithDecryption=True)
@@ -9,7 +9,7 @@ def get_regin_name():
         return 'sa-east-1'
     
 def get_ssm_parameter(name, default=None):
-    ssm = boto3.client('ssm', get_regin_name())
+    ssm = boto3.client('ssm', get_region_name())
     try:
         parameter = ssm.get_parameter(Name=name, WithDecryption=True)
         return parameter['Parameter']['Value']
